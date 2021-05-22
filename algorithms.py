4c0de595ab7dde8d5f4101aa41d6b6716cc64be0
fa.benavideso@uniandes.edu.co <fa.benavideso@uniandes.edu.co>
2021-05-22 13:03:05 -0500

    [added] lists and algorithms

---
+++ algorithms.py
@@ -0,0 +1,12 @@
+# Bubble sort
+my_list = [8, 10, 6, 2, 4]  # list to sort
+swapped = True  # It's a little fake, we need it to enter the while loop.
+
+while swapped:
+    swapped = False  # no swaps so far
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True  # a swap occurred!
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print(my_list)
--- lists.py
+++ lists.py
@@ -1,13 +1,37 @@
 print("Using a list")
 
-mList = [10, 5, 7, 2, 1]
-print(mList)
+my_list = [10, 5, 7, 2, 1]
+print(my_list)
 
-del mList[1]    # Removes 5 
-print(mList)    # [10, 7, 2, 1]
+del my_list[1]    # Removes 5 
+print(my_list)    # [10, 7, 2, 1]
 
-mList.append(88)	# add 88 at the end
-print(mList)    # [10, 7, 2, 1, 88]
+my_list.append(88)	# add 88 at the end
+print(my_list)    # [10, 7, 2, 1, 88]
 
-mList.insert(0, 0)  # Add 0 to first position
-print(mList)    # [0, 10, 7, 2, 1, 88]
+my_list.insert(0, 0)  # Add 0 to first position
+print(my_list)    # [0, 10, 7, 2, 1, 88]
+
+print("\nSorting Lists")
+sortedList = my_list.sort()
+print(my_list)    # The list is sorted on same memory space [0, 1, 2, 7, 10, 88]
+print(sortedList)   # no new list will be generated
+my_list.reverse()
+print(my_list)    # [88, 10, 7, 2, 1, 0]
+
+print("\nCopying some part of the list.")
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:3] # it creates a new list
+print(new_list) #[8,6]
+
+print("\nUsing in Operator")
+my_list = [-2, 3, 7, 0, 4]
+print(5 in my_list)		# False
+print(5 not in my_list)	# True
+print(0 in my_list)	# True
+
+print("\nCreating Lists")
+star = "*"
+print([star for i in range(5)])     # ['*', '*', '*', '*', '*'] 1d
+print([[star for j in range(2)] for k in range(3)])	# it Creates a matriz 3x2 2d
+print([[[star for r in range(1)] for f in range(2)] for t in range(3)])  # 3x2x1 3d
