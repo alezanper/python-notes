21a09c11e0a4def299d7b35f29e872d36c0a1da6
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-08-29 17:02:51 -0500

    [added] restful flask updated

---
+++ Flask/code/app2.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name']==name, items), None)
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        return {'item': item}, 200 if item is not None else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name']==name, items), None):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+        data = request.get_json()   # force=True silence=True
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')    
+
+
+app.run(port=5000, debug=True)
