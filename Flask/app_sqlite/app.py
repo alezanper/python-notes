b18771ecc95d1500163c45e56c66d8f579768b04
fabenavideso@gmail.com <fabenavideso@gmail.com>
2021-08-30 21:29:36 -0500

    [added] app with database

---
+++ Flask/app_sqlite/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'alex'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+items = []
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required= True,
+        help="This field cannot be left blank"
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name']==name, items), None)
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        return {'item': item}, 200 if item is not None else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name']==name, items), None):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
+        data = Item.parser.parse_args()
+
+        #data = request.get_json()   # force=True silence=True
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name']!=name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # data = request.get_json()   # force=True silence=True
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')    
+
+app.run(port=5000, debug=True)
---
+++ Flask/app_sqlite/user.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+
+class User:
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            #user = cls(row[0], row[1], row[2])
+            user = cls(*row)
+
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            #user = cls(row[0], row[1], row[2])
+            user = cls(*row)
+
+        else:
+            user = None
+
+        connection.close()
+        return user
