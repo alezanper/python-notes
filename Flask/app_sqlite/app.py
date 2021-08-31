90c1733cdfa4f3952c6f48dd6ae0f681f2d6ea28
fabenavideso@gmail.com <fabenavideso@gmail.com>
2021-08-30 22:15:50 -0500

    [updated] table configuration

--- Flask/app_sqlite/app.py
+++ Flask/app_sqlite/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'alex'
@@ -60,5 +61,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')    
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
--- Flask/app_sqlite/user.py
+++ Flask/app_sqlite/user.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+from typing_extensions import Required
+from flask_restful import Resource, reqparse
 
 class User:
     def __init__(self, _id, username, password):
@@ -42,3 +43,31 @@ class User:
 
         connection.close()
         return user
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type = str,
+        required = True,
+        help = 'This field cannot be blank.'
+    )
+    parser.add_argument('password',
+        type = str,
+        required = True,
+        help = 'This field cannot be blank.'
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+
+        connection.close()
+
