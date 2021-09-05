549cfebc1762068eccb3bae56b0bfd1f68eef42c
fabenavideso@gmail.com <fabenavideso@gmail.com>
2021-09-04 21:19:24 -0500

    [added] get method with sqlite

---
+++ Flask/app_sqlite/item.py
@@ -0,0 +1,55 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+import sqlite3
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required= True,
+        help="This field cannot be left blank"
+    )
+
+    @jwt_required()
+    def get(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
+        return {'message': 'Item not found'}, 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name']==name, items), None):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
+        data = Item.parser.parse_args()
+
+        #data = request.get_json()   # force=True silence=True
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name']!=name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        # data = request.get_json()   # force=True silence=True
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
