22219190cfa79471cc0db7d3cb72cf6b0d01bc5e
fabenavideso@gmail.com <fabenavideso@gmail.com>
2021-09-04 22:43:26 -0500

    [added] update item

--- Flask/app_sqlite/item.py
+++ Flask/app_sqlite/item.py
@@ -10,7 +10,7 @@ class Item(Resource):
         help="This field cannot be left blank"
     )
 
-    @jwt_required()
+    #@jwt_required()
     def get(self, name):
         item = self.find_by_name(name)
         if item:
@@ -39,6 +39,15 @@ class Item(Resource):
         #data = request.get_json()   # force=True silence=True
         item = {'name': name, 'price': data['price']}
 
+        try:
+            self.insert(item)
+        except:
+            return {'message': 'An error accured inserting the item.'}, 500
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -47,8 +56,6 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
-
-        return item, 201
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
@@ -64,14 +71,33 @@ class Item(Resource):
 
     def put(self, name):
         data = Item.parser.parse_args()
-        # data = request.get_json()   # force=True silence=True
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
+
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {'message': 'An error accured inserting the item.'}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                return {'message': 'An error accured updating the item.'}, 500        
+        return updated_item
+
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
+
 class ItemList(Resource):
     def get(self):
         return {'items': items}
