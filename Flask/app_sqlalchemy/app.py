c9885704dbcf7cdfead4fd4031679c83062865ad
fabenavideso@gmail.com <fabenavideso@gmail.com>
2021-09-06 19:30:42 -0500

    [added] sql alchemy

---
+++ Flask/app_sqlalchemy/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from user import UserRegister
+
+from item import Item, ItemList
+
+app = Flask(__name__)
+app.secret_key = 'alex'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')    
+api.add_resource(UserRegister, '/register')
+
+# Only executes when a user run this file (python app.py)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
