2060e77b551f2c19fbd384713aac9b3ba2bc06ea
fabenavideso@gmail.com <fabenavideso@gmail.com>
2022-07-31 20:06:14 -0500

    [added] operators

--- Basics/sets.py
+++ Basics/sets.py
@@ -10,3 +10,18 @@ print(s)
 
 hello = "hello"
 print(set(hello))   # {'h', 'e', 'l', 'o'}
+
+print("\nUsing Operators")
+a = {1,2,3,4,5,6,7,8,9}
+b = {4,3,6,9}
+print(a)
+print(b)
+print(a==b) # False
+print(a!=b) # True
+print(a<=b) # False (a is a subset of b?)
+print(b<=a) # True (b is a subset of a?)
+print(a>=b) # True (a is a superset of b?)
+print(a|b) # Union {1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(a&b) # Intersection {9, 3, 4, 6}
+print(a-b) # set of elements in a but not in b {1, 2, 5, 7, 8}
+
