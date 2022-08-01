54d35553eb57dadddb02d546d9afe60e894a8259
fabenavideso@gmail.com <fabenavideso@gmail.com>
2022-07-31 19:57:02 -0500

    [added] constructor operators

--- Basics/dictionary.py
+++ Basics/dictionary.py
@@ -34,7 +34,19 @@ print("\nUsing items function with dictionary")
 for key, value in dictionary.items():
     print(key, "->", value)
 
-# Nested Ddictionaries
+# Nested Dictionaries
 print("\nUsing nested dictionary")
 d = {'key': ['a', 'b', 'c']}
 print(d['key'][1])
+
+# Operators
+print("\nOperators")
+a = [("sp", "Spanish"),("en", "English")]
+b = [("du", "Russian"),("en", "English"), ("jp", "Japanese")]
+dict_a = dict(a)
+dict_b = dict(b)
+print(dict_a)
+print(dict_b)
+print(dict_a == dict_b) # False (is equivalent?)
+print(dict_a != dict_b) # True (is not equivalent?)
+
