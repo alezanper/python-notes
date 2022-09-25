7531ab19fa5332e184a1422857a1c97bb4d71b33
fabenavideso@gmail.com <fabenavideso@gmail.com>
2022-09-25 17:02:00 -0500

    [updated]

--- Basics/comprehesion_syntax.py
+++ Basics/comprehesion_syntax.py
@@ -1,20 +1,24 @@
-print("Creating a List")
-list = [i for i in range(10)]
-print(list) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print()
+def test_comprehesion():
+    print("Creating a List")
+    list = [i for i in range(10)]
+    print(list) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print()
 
-print("Creating a Set")
-set = {i for i in range(10)}
-print(set) # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-print()
+    print("Creating a Set")
+    set = {i for i in range(10)}
+    print(set) # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+    print()
 
-print("Creating a Generator")   # when results do not need to be stored in memory
-generator = (i for i in range(10))
-print(sum(generator))
-print(generator) 
-print()
+    print("Creating a Generator")   # when results do not need to be stored in memory
+    generator = (i for i in range(10))
+    print(sum(generator))
+    print(generator) 
+    print()
 
-print("Creating a Dictionary")
-dictionary = {i:i*i for i in range(10)}
-print(dictionary) 
-print()
+    print("Creating a Dictionary")
+    dictionary = {i:i*i for i in range(10)}
+    print(dictionary) 
+    print()
+
+if __name__ == "__main__":
+    test_comprehesion()
